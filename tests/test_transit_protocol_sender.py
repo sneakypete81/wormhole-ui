4c1a65bfe0a3adaaf73a7e40e39417ece7c8a9cf
sneakypete81 <sneakypete81@gmail.com>
2020-02-13 22:22:58 +0000

    Remove reactor from transit test

--- tests/test_transit_protocol_sender.py
+++ tests/test_transit_protocol_sender.py
@@ -1,9 +1,5 @@
-from unittest import mock
-
-# from hamcrest import assert_that, is_, starts_with
 import pytest
-# import pytest_twisted
-from twisted.internet import task, defer
+from twisted.internet import defer
 
 from wormhole_ui.transit_protocol_sender import TransitProtocolSender
 
@@ -14,20 +10,16 @@ class TestTransitProtocolSender:
         self.transit = mocker.patch(
             "wormhole_ui.transit_protocol_sender.TransitSender"
         )()
+        self.wormhole = mocker.Mock()
 
-    def test_send_file_sends_transit(self):
-        wormhole = mock.Mock()
-        clock = task.Clock()
-
-        d = defer.Deferred()
-        clock.callLater(1, d.callback, "hints")
-        self.transit.get_connection_hints.return_value = d
+    def test_send_file_sends_transit(self, mocker):
+        self.transit.get_connection_hints.return_value = defer.Deferred()
         self.transit.get_connection_abilities.return_value = "abilities"
 
-        transit_sender = TransitProtocolSender(clock, wormhole, None)
-        transit_sender.send_file(mock.Mock())
+        transit_sender = TransitProtocolSender(None, self.wormhole, None)
+        transit_sender.send_file(mocker.Mock())
+        self.transit.get_connection_hints.return_value.callback("hints")
 
-        clock.advance(1)
-        wormhole.send_message.assert_called_with(
+        self.wormhole.send_message.assert_called_with(
             b'{"transit": {"abilities-v1": "abilities", "hints-v1": "hints"}}',
         )
