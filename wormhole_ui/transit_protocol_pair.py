d3010b88a4b0a76b700ee33d492757956c86bc7d
sneakypete81 <sneakypete81@gmail.com>
2020-02-17 14:58:57 +0000

    Refactor state out of TransitProtocolReceiver

--- wormhole_ui/transit_protocol_pair.py
+++ wormhole_ui/transit_protocol_pair.py
@@ -1,5 +1,6 @@
 import logging
 
+from .dest_file import DestFile
 from .source_file import SourceFile
 from .transit_protocol_sender import TransitProtocolSender
 from .transit_protocol_receiver import TransitProtocolReceiver
@@ -7,14 +8,17 @@ from .transit_protocol_receiver import TransitProtocolReceiver
 
 class TransitProtocolPair:
     def __init__(self, reactor, wormhole, delegate):
-        self.receiver = TransitProtocolReceiver(reactor, wormhole, delegate)
+        self._receiver = TransitProtocolReceiver(reactor, wormhole, delegate)
         self._sender = TransitProtocolSender(reactor, wormhole, delegate)
 
         self._source_file = None
+        self._dest_file = None
 
         self._send_transit_handshake_complete = False
+        self._receive_transit_handshake_complete = False
         self._awaiting_transit_response = False
         self.is_sending_file = False
+        self.is_receiving_file = False
 
     def send_file(self, id, file_path):
         logging.debug("TransitProtocolPair::send_file")
@@ -45,8 +49,14 @@ class TransitProtocolPair:
             self._sender.send_offer(self._source_file)
 
         else:
-            # We've received a transit message first, so this is for the receiver
-            self.receiver.handle_transit(transit_message)
+            # We haven't sent a transit message, so this is for the receiver
+            assert not self.is_receiving_file
+
+            if not self._receive_transit_handshake_complete:
+                self._receive_transit_handshake_complete = True
+                self._receiver.handle_transit(transit_message)
+
+            self._receiver.send_transit()
 
     def handle_file_ack(self):
         logging.debug("TransitProtocolPair::handle_file_ack")
@@ -58,12 +68,35 @@ class TransitProtocolPair:
 
         self._sender.send_file(self._source_file, on_send_finished)
 
+    def handle_offer(self, offer):
+        logging.debug("TransitProtocolPair::handle_offer")
+        assert not self.is_receiving_file
+
+        filename = self._receiver.handle_offer(offer)
+        self._dest_file = DestFile(filename)
+        return self._dest_file
+
+    def receive_file(self, id, dest_path):
+        logging.debug("TransitProtocolPair::receive_file")
+        assert not self.is_receiving_file
+        self.is_receiving_file = True
+
+        def on_receive_finished():
+            self.is_receiving_file = False
+            self._dest_file.cleanup()
+            self._dest_file = None
+
+        self._dest_file.open(id, dest_path)
+        self._receiver.receive_file(self._dest_file, on_receive_finished)
 
     def close(self):
         self._source_file = None
+        self._dest_file = None
         self._send_transit_handshake_complete = False
+        self._receive_transit_handshake_complete = False
         self._awaiting_transit_response = False
         self.is_sending_file = False
+        self.is_receiving_file = False
 
         self._sender.close()
-        self.receiver.close()
+        self._receiver.close()
--- wormhole_ui/transit_protocol_receiver.py
+++ wormhole_ui/transit_protocol_receiver.py
@@ -8,7 +8,6 @@ from wormhole.cli import public_relay
 from wormhole.transit import TransitReceiver
 
 from .transit_protocol_base import TransitProtocolBase
-from .dest_file import DestFile
 from .errors import (
     OfferError,
     ReceiveFileError,
@@ -26,75 +25,48 @@ class TransitProtocolReceiver(TransitProtocolBase):
 
         self._send_transit_deferred = None
         self._receive_file_deferred = None
-        self._transit_handshake_complete = False
-        self._dest = None
         self._pipe = None
 
-        self.is_receiving_file = False
-
-    def handle_transit(self, transit_message):
-        logging.debug("TransitProtocolReceiver::handle_transit")
-        assert not self.is_receiving_file
-
-        if not self._transit_handshake_complete:
-            self._transit_handshake_complete = True
-            super().handle_transit(transit_message)
-
-        self.send_transit()
-
     def handle_offer(self, offer):
-        assert not self.is_receiving_file
-
-        if "file" in offer:
-            self._dest = DestFile(offer["file"])
-            return self._dest
-
-        else:
+        if "file" not in offer:
             raise RespondError(OfferError(f"Unknown offer: {offer}"))
 
-    def receive_file(self, id, dest_path):
-        assert self._dest is not None
-        assert not self.is_receiving_file
+        return offer["file"]
 
+    def receive_file(self, dest_file, receive_finished_handler):
         self._send_data({"answer": {"file_ack": "ok"}})
-        self._dest.open(id, dest_path)
 
-        self._receive_file_deferred = self._receive_file()
+        self._receive_file_deferred = self._receive_file(dest_file)
         self._receive_file_deferred.addErrback(self._on_deferred_error)
+        self._receive_file_deferred.addBoth(lambda _: receive_finished_handler())
 
     @defer.inlineCallbacks
-    def _receive_file(self):
-        self.is_receiving_file = True
-        try:
-            if self._pipe is None:
-                self._pipe = yield self._transit.connect()
+    def _receive_file(self, dest_file):
+        if self._pipe is None:
+            self._pipe = yield self._transit.connect()
 
-            datahash = yield self._transfer_data(self._pipe)
-            self._dest.finalise()
-            yield self._close_transit(self._pipe, datahash)
+        datahash = yield self._transfer_data(self._pipe, dest_file)
+        dest_file.finalise()
+        yield self._close_transit(self._pipe, datahash)
 
-            logging.info("File received, transfer complete")
-        finally:
-            self.is_receiving_file = False
-            self._dest.cleanup()
-
-        self._delegate.transit_complete(self._dest.id, self._dest.name)
+        logging.info("File received, transfer complete")
+        self._delegate.transit_complete(dest_file.id, dest_file.name)
 
     @defer.inlineCallbacks
-    def _transfer_data(self, pipe):
+    def _transfer_data(self, pipe, dest_file):
         hasher = hashlib.sha256()
-        progress = Progress(self._delegate, self._dest.id, self._dest.transfer_bytes)
+        progress = Progress(self._delegate, dest_file.id, dest_file.transfer_bytes)
         received = yield pipe.writeToFile(
-            self._dest.file_object,
-            self._dest.transfer_bytes,
+            dest_file.file_object,
+            dest_file.transfer_bytes,
             progress=progress.update,
             hasher=hasher.update,
         )
         datahash = hasher.digest()
 
-        if received < self._dest.transfer_bytes:
+        if received < dest_file.transfer_bytes:
             raise ReceiveFileError("Connection dropped before full file received")
-        assert received == self._dest.transfer_bytes
+        assert received == dest_file.transfer_bytes
         return datahash
 
     @defer.inlineCallbacks
