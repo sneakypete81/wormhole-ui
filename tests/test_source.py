7086b4f7ed6408c8ecb73af42a37af9e61b90dd8
sneakypete81 <sneakypete81@gmail.com>
2020-03-09 19:32:13 +0000

    Add SourceDir for sending zipped directories

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,14 +1,20 @@
 from pathlib import Path
+import zipfile
 
-from hamcrest import assert_that, is_, ends_with
+from hamcrest import assert_that, is_, ends_with, has_length
 import pytest
 
-from wormhole_ui.protocol.transit.source import SourceFile
+from wormhole_ui.protocol.transit.source import SourceDir, SourceFile
 
 
 @pytest.fixture
 def test_file_path():
     return str(Path(__file__).parent / "test_files" / "file.txt")
+
+
+@pytest.fixture
+def test_dir_path():
+    return str(Path(__file__).parent / "test_files")
 
 
 class TestSourceFile:
@@ -32,3 +38,40 @@ class TestSourceFile:
 
         assert_that(source_file.transfer_bytes, is_(33))
         assert_that(source_file.final_bytes, is_(33))
+
+
+class TestSourceDir:
+    def test_attributes_are_set(self, test_dir_path):
+        source_dir = SourceDir(13, test_dir_path)
+
+        assert_that(source_dir.id, is_(13))
+        assert_that(source_dir.name, is_("test_files"))
+
+    def test_open_creates_a_zipfile_object(self, test_dir_path):
+        source_dir = SourceDir(13, test_dir_path)
+
+        source_dir.open()
+
+        assert_that(source_dir.file_object.closed, is_(False))
+        assert_that(source_dir.num_files, is_(2))
+
+    def test_open_gets_file_size(self, test_dir_path):
+        source_dir = SourceDir(13, test_dir_path)
+
+        source_dir.open()
+
+        assert_that(source_dir.final_bytes, is_(67))
+        assert_that(source_dir.transfer_bytes, is_(288))
+
+    def test_open_zips_directory_contents(self, test_dir_path):
+        source_dir = SourceDir(13, test_dir_path)
+
+        source_dir.open()
+
+        with zipfile.ZipFile(source_dir.file_object) as zf:
+            assert_that(zf.infolist(), has_length(2))
+            assert_that(zf.read("file.txt"), is_(b"This is a file used for testing.\n"))
+            assert_that(
+                zf.read("subdir/file2.txt"),
+                is_(b"This is a file in a subdirectory.\n"),
+            )
--- wormhole_ui/protocol/transit/source.py
+++ wormhole_ui/protocol/transit/source.py
@@ -1,27 +1,31 @@
+import os
 from pathlib import Path
+import tempfile
+import zipfile
 
 from ...errors import SendFileError
 
 
-def source_factory(id, file_path):
-    if file_path.is_file():
-        return SourceFile(id, file_path)
-    elif file_path.is_dir():
-        return SourceDir(id, file_path)
+def source_factory(id, path):
+    if Path(path).is_file():
+        return SourceFile(id, path)
+    elif Path(path).is_dir():
+        return SourceDir(id, path)
 
-    raise SendFileError("Only files or directories can be sent")
+    raise SendFileError("Can only send files or directories")
 
 
 class Source:
-    def __init__(self, id, file_path):
-        file_path = Path(file_path).resolve()
-        assert file_path.exists()
+    def __init__(self, id, path):
+        path = Path(path).resolve()
+        assert path.exists()
 
         self.id = id
-        self.name = file_path.name
-        self.full_path = file_path
+        self.name = path.name
+        self.full_path = path
         self.final_bytes = None
         self.transfer_bytes = None
+        self.num_files = None
         self.file_object = None
 
     def open(self):
@@ -35,8 +39,38 @@ class SourceFile(Source):
         self.final_bytes = self.file_object.tell()
         self.transfer_bytes = self.final_bytes
         self.file_object.seek(0, 0)
+        self.num_files = 1
 
 
 class SourceDir(Source):
     def open(self):
-        raise NotImplementedError
+        self.file_object = tempfile.SpooledTemporaryFile()
+
+        # workaround for https://bugs.python.org/issue26175 (STF doesn't
+        # fully implement IOBase abstract class), which breaks the new
+        # zipfile in py3.7 that expects .seekable
+        if not hasattr(self.file_object, "seekable"):
+            # AFAICT all the filetypes that STF wraps can seek
+            self.file_object.seekable = lambda: True
+
+        self._build_zipfile()
+        self.file_object.seek(0, 2)
+        self.transfer_bytes = self.file_object.tell()
+        self.file_object.seek(0, 0)
+
+    def _build_zipfile(self):
+        self.num_files = 0
+        self.final_bytes = 0
+
+        with zipfile.ZipFile(
+            self.file_object, "w", compression=zipfile.ZIP_DEFLATED, allowZip64=True
+        ) as zf:
+            filepaths = self.full_path.rglob("*")
+            for filepath in [f for f in filepaths if f.is_file()]:
+                try:
+                    zf.write(filepath, filepath.relative_to(self.full_path))
+                    self.final_bytes += os.stat(filepath).st_size
+                    self.num_files += 1
+                except OSError as e:
+                    errmsg = u"{}: {}".format(filepath, e.strerror)
+                    raise SendFileError(errmsg)
