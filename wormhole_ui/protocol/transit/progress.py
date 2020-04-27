c4d43d92d989ae888570ab697e6b9ed503c33dbb
sneakypete81 <sneakypete81@gmail.com>
2020-04-27 20:46:38 +0100

    Don't send progress signals too regularly
    This improves throughput when progress.update() is called on each
    signal.

--- wormhole_ui/protocol/transit/progress.py
+++ wormhole_ui/protocol/transit/progress.py
@@ -1,12 +1,22 @@
+import time
+
+UPDATE_RATE_SECONDS = 0.1
+
+
 class Progress:
     def __init__(self, delegate, id, total_bytes):
         self._delegate = delegate
         self._id = id
         self._total_bytes = total_bytes
         self._transferred_bytes = 0
+        self._last_update_sent = 0
 
     def update(self, increment_bytes):
         self._transferred_bytes += increment_bytes
-        self._delegate.transit_progress(
-            self._id, self._transferred_bytes, self._total_bytes
-        )
+        now = time.monotonic()
+
+        if now - self._last_update_sent > UPDATE_RATE_SECONDS:
+            self._delegate.transit_progress(
+                self._id, self._transferred_bytes, self._total_bytes
+            )
+            self._last_update_sent = now
